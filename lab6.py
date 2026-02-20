67dbddb4fc17d33a7643bf51bcc64521e5cee8d5
Ishan <agarwal.ishanedu@gmail.com>
2026-02-20 17:18:52 -0500

    Added checkout and view cart options

--- lab6.py
+++ lab6.py
@@ -1,15 +1,33 @@
 """
 Program Name: Lab 6
 My name: Ishan Agarwal
-Purpose: 
+Purpose: This lab is meant to deepen my understanding of dictionaries.
 Starter Code: None
 Date: 02/20/2026
 """
 
-cart: dict[str, int] = {}
+my_cart: dict[str, int] = {}
 
 # Introduction + Instructions
 print("Welcome to the store!")
 print("Enter an item, or type 'view' to see your cart, or 'checkout' to finish.")
 print("")
+
+while True:
+    # Ask about what action they take
+    item: str = input("What would you like? ").lower()
+
+    # Check if they checkout
+    if item == "checkout":
+        break
+
+    # Check if they want to view their cart
+    elif item == "view":
+        print("-- YOUR CART --")
+        if not my_cart:
+            print("Your cart is empty.")
+        else:
+            for key, value in my_cart.items():
+                print(f"{key}: {value}")
+        print("---------------")
 
