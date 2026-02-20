600a8a38bb1c3b8c809becfeaef1aa7f2bfb45d2
Ishan <agarwal.ishanedu@gmail.com>
2026-02-20 16:52:39 -0500

    Revision: This sets up an empty cart + Intro/Instructions

---
+++ lab6.py
@@ -0,0 +1,15 @@
+"""
+Program Name: Lab 6
+My name: Ishan Agarwal
+Purpose: 
+Starter Code: None
+Date: 02/20/2026
+"""
+
+cart: dict[str, int] = {}
+
+# Introduction + Instructions
+print("Welcome to the store!")
+print("Enter an item, or type 'view' to see your cart, or 'checkout' to finish.")
+print("")
+
